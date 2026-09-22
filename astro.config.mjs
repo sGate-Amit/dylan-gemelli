cbbde56b0ec78c070ce975e817ebfb45ae845bc4
sGate-Amit <105426175+sGate-Amit@users.noreply.github.com>
2026-09-22 11:35:32 +0530

    Update astro.config.mjs
    file updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import cloudflare from "@astrojs/cloudflare";
-
 export default defineConfig({
   site: "https://dylangemelli.com/",
   integrations: [mdx(), sitemap()],
